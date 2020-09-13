a48bb29a017a07ca8b06d22145897bb108fc534d
Min-Chang-Kyeoung <janu723@naver.com>
2020-09-13 22:07:28 +0900

    insertUser 함수 버그 수정

--- src/common/conncetion/Connection.ts
+++ src/common/conncetion/Connection.ts
@@ -2,15 +2,15 @@ import { Mongoose, connect } from 'mongoose';
 let isConnected: boolean = false;
 
 export const connectToDatabase = () => {
-    console.log('Start connecting db...');
+
     if (isConnected) {
         return Promise.resolve();
     }
     console.log('env:', process.env.db);
     const defaultDb = process.env.db || 'mongodb+srv://janu723:changk1325!@gamepartener.pp7ww.mongodb.net/gamepartner?retryWrites=true&w=majority';
     const dbUri: string = defaultDb;
-    console.log(dbUri);
-    return connect(dbUri).then((db: Mongoose) => {
+
+    return connect(dbUri,{useNewUrlParser:true,useUnifiedTopology: true}).then((db: Mongoose) => {
         isConnected = db.connection.readyState == 1; // 1 for connected
     }).catch(error => {
         console.log('db error:', error);

e70463c933927296a35fe6b21bf7c9f1d5a1786c
Min-Chang-Kyeoung <janu723@naver.com>
2020-09-16 23:39:33 +0900

    s3 파일업로드 개발중

--- src/handler/UserHandler.ts
+++ src/handler/UserHandler.ts
@@ -26,18 +26,22 @@ export const insertUser: APIGatewayProxyHandler = async (event, _context) => {
 export const insertImage: APIGatewayProxyHandler = async (event, _context) => {
   _context.callbackWaitsForEmptyEventLoop = false;
   try{
-    const s3 = new AWS.S3();
-    const params = JSON.parse(event.body);
-    console.log(params);
-    
-    const s3Params = {
+    const s3 = await new AWS.S3();
+
+    const params = await JSON.parse(event.body);
+    const s3Params = await {
       Bucket: 'gamepartner',
       Key: params.name,
       ContentType: 'multipart/form-data',
       ACL: 'public-read'
     }
-  
-    const uploadURL = s3.getSignedUrl('putObject', s3Params)
+
+    console.log(params);
+    
+    console.log(s3Params);
+    
+
+    const uploadURL = await s3.getSignedUrlPromise('putObject', s3Params);
 
     return response(200,{
       result: true,
